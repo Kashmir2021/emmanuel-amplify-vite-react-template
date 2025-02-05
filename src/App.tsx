15d12754b5ceecb9c87f132b19857da6d364a3ed
Kashmir2021 <144963971+Kashmir2021@users.noreply.github.com>
2025-02-05 14:55:15 -0700

    added per-user data isolation.1

--- src/App.tsx
+++ src/App.tsx
@@ -39,8 +39,7 @@ function App() {
         🥳 App successfully hosted. Try creating a new todo.
         <br />
         <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
+            </a>
         <button onClick={signOut}>Sign out</button>
       </div>
     </main>

9a959e911f4427b4bf9715acf174ae5bd9dbd33d
Kashmir2021 <144963971+Kashmir2021@users.noreply.github.com>
2025-02-06 09:47:42 -0700

    added tags

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,22 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+//Tags
+import { Tags } from 'aws-cdk-lib';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+const tags = Tags.of(backend.stack);
+// add a new tag
+// Example tags.add('my-key', 'my-value');
+   tags.add('contact', 'Emmanuel Sedas');
+   tags.add('dept', 'DTS');
+   tags.add('elcid', 'itclarch');
+   tags.add('env', 'test');
+   tags.add('app', 'EvalAmpllify');
+   tags.add('security', '0');
+   // remove tags
+//tags.remove('my-key');
